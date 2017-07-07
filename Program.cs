56e31e5a7984f933869f8886e5d82b2648d2ac2d
Eduardo Carranza <ecarranza@pagroupco.com>
2017-07-07 16:43:14 -0400

    Implementation of the method Move_File

--- Program.cs
+++ Program.cs
@@ -41,13 +41,16 @@ namespace Migration_1._0
             Console.ReadLine();
         }
 
-        static void Process_Claim(string a, string b , string c)
+        static void Process_Claim(string origin, string logfile , string destination)
         {
             //Open and Read the File .log
-            var migrated_documentos = File.ReadAllLines(b);
+            var migrated_documentos = File.ReadAllLines(logfile);
+
+            //To get the Total of migrated files
             int migrated_documents_total = Convert.ToInt32(migrated_documentos[migrated_documentos.Length - 2].Split(' ')[4]);
             int current_document = 0;
 
+            //To get the migrated file Names
             var query = (from documents in migrated_documentos
                          where documents.Contains("File to migrate:")
                          select documents).ToArray();
@@ -55,24 +58,11 @@ namespace Migration_1._0
             foreach (var item in query)
             {
                 int substring_count = item.Split(' ').Length;
-                string File_to_Find = a + @"\" + item.Split(' ')[substring_count - 1];
+                string File_to_Find = origin + @"\" + item.Split(' ')[substring_count - 1];
 
                 try
                 {
-                    FileInfo file = new FileInfo(File_to_Find);
-                    if (file.Exists)
-                    {
-                        current_document++;
-                        string archivo_destino = c + @"\" + file.Name;
-                        Console.WriteLine("File to move : {0}", file.Name);
-                        file.MoveTo(archivo_destino);
-                        Console.WriteLine("The file has been moved");
-                        Console.WriteLine("{0}%", current_document * 100 / migrated_documents_total);
-                    }
-                    else
-                    {
-                        Console.WriteLine("The file : {0} doesn't exist", file.Name);
-                    }
+                    Move_File(destination, migrated_documents_total, ref current_document, File_to_Find);
                 }
                 catch (Exception error)
                 {
@@ -88,12 +78,16 @@ namespace Migration_1._0
             else
                 Console.WriteLine("There is no files to migrate");
         }
-        static void Process_Policies_and_Agents(string a, string b, string c)
+        static void Process_Policies_and_Agents(string origin, string logfile, string destination)
         {
-            var migrated_documentos = File.ReadAllLines(b);
+            //Open and Read the File .log
+            var migrated_documentos = File.ReadAllLines(logfile);
+
+            //To get the Total of migrated files
             int migrated_documents_total = Convert.ToInt32(migrated_documentos[migrated_documentos.Length - 2].Split(' ')[4]);
             int current_document = 0;
 
+            //To get the migrated file Names
             var query = (from documents in migrated_documentos
                          where documents.Contains("successfully uploaded to DMS")
                          select documents).ToArray();
@@ -102,25 +96,11 @@ namespace Migration_1._0
             {
                 string temp = item.Split(' ')[5];
                 string File_Name = temp.Substring(1, temp.Length - 2);
-                string File_to_Find = a + @"\" + File_Name;
+                string File_to_Find = origin + @"\" + File_Name;
 
                 try
                 {
-                    FileInfo file = new FileInfo(File_to_Find);
-                    if (file.Exists)
-                    {
-                        current_document++;
-                        string archivo_destino = c + @"\" + file.Name;
-                        Console.WriteLine("File to move : {0}", file.Name);
-                        file.MoveTo(archivo_destino);
-                        Console.WriteLine("The file has been moved");
-                        Console.WriteLine("{0}%", current_document * 100 / migrated_documents_total);
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("The file : {0} doesn't exist", file.Name);
-                    }
+                    Move_File(destination, migrated_documents_total, ref current_document, File_to_Find);
                 }
                 catch (Exception error)
                 {
@@ -135,14 +115,16 @@ namespace Migration_1._0
             else
                 Console.WriteLine("There is no files to migrate");
         }
-        static void Process_Precerts(string a , string b , string c)
+        static void Process_Precerts(string origin , string logfile , string destination)
         {
+            //Open and Read the File .log
+            var migrated_documentos = File.ReadAllLines(logfile);
 
-            var migrated_documentos = File.ReadAllLines(b);
-            
+            //To get the Total of migrated files
             int migrated_documents_total = Convert.ToInt32(migrated_documentos[migrated_documentos.Length - 4].Split(' ')[6]);
             int current_document = 0;
 
+            //To get the migrated file Names
             var query = (from documents in migrated_documentos
                          where documents.Contains("Loading file:")
                          select documents).ToArray();
@@ -151,32 +133,16 @@ namespace Migration_1._0
             {
                 string temp = item.Split(' ')[item.Split(' ').Length - 1];
                 string File_Name = temp.Substring(1, temp.Length - 2);
-                string File_to_Find = a + @"\" + File_Name;
+                string File_to_Find = origin + @"\" + File_Name;
 
                 try
                 {
-                    FileInfo file = new FileInfo(File_to_Find);
-                    if (file.Exists)
-                    {
-                        current_document++;
-                        string archivo_destino = c + @"\" + file.Name;
-                        Console.WriteLine("File to move : {0}", file.Name);
-                        file.MoveTo(archivo_destino);
-                        Console.WriteLine("The file has been moved");
-                        Console.WriteLine("{0}%", current_document * 100 / migrated_documents_total);
-                    }
-                    else
-                    {
-                        Console.WriteLine("The file : {0} doesn't exist", file.Name);
-                    }
-
+                    Move_File(destination, migrated_documents_total, ref current_document, File_to_Find);
                 }
                 catch (Exception error)
                 {
                     Console.WriteLine(error.Message);
                 }
-
-
             }
 
             
@@ -186,6 +152,24 @@ namespace Migration_1._0
             }
             else
                 Console.WriteLine("There is no files to migrate");
+        }
+
+        private static void Move_File(string destination, int migrated_documents_total,ref int current_document, string File_to_Find)
+        {
+            FileInfo file = new FileInfo(File_to_Find);
+            if (file.Exists)
+            {
+                current_document++;
+                string archivo_destino = destination + @"\" + file.Name;
+                Console.WriteLine("File to move : {0}", file.Name);
+                file.MoveTo(archivo_destino);
+                Console.WriteLine("The file has been moved");
+                Console.WriteLine("{0}%", current_document * 100 / migrated_documents_total);
+            }
+            else
+            {
+                Console.WriteLine("The file : {0} doesn't exist", file.Name);
+            }
 
         }
     }
